ad65c8a48eea5ae69778a768773c0e84f904f358
hmandevteam <hmandevteam@2bb0322c-6031-0410-b042-1f6d2b638ab5>
2009-07-05 15:25:53 +0000

    * monosim/src/AssemblyInfo.cs: * monopcsclib/monopcsclib.csproj:
    - using one class for all os (dllmap added in app.config)

--- trunk/monosim/src/AssemblyInfo.cs
+++ trunk/monosim/src/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.CompilerServices;
 // change them to the information which is associated with the assembly
 // you compile.
 
-[assembly: AssemblyTitle("monoSIM")]
+[assembly: AssemblyTitle("monosim")]
 [assembly: AssemblyDescription("manage your phonebook on your sim card with standard pcsc smartcard reader")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Armando Basile")]
-[assembly: AssemblyProduct("monoSIM")]
+[assembly: AssemblyProduct("monosim")]
 [assembly: AssemblyCopyright("Armando Basile 2007-2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -23,7 +23,7 @@ using System.Runtime.CompilerServices;
 // You can specify all values by your own or you can build default build and revision
 // numbers with the '*' character (the default):
 
-[assembly: AssemblyVersion("1.4.3.36")]
+[assembly: AssemblyVersion("1.4.4.40")]
 
 // The following attributes specify the key for the sign of your assembly. See the
 // .NET Framework documentation for more information about signing.
