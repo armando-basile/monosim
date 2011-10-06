728673f8c6898dcb0627ffc6b74d61340f804a4d
hmandevteam@gmail.com <hmandevteam@gmail.com@2bb0322c-6031-0410-b042-1f6d2b638ab5>
2011-10-06 21:14:18 +0000

    - first sim commands implemented

--- trunk/monosim-gtk/Src/Gui_Classes/MainWindowClass.cs
+++ trunk/monosim-gtk/Src/Gui_Classes/MainWindowClass.cs
@@ -20,13 +20,14 @@ namespace monosimgtk
 	{
 		
 		
-		
-		
-		
-		
-		
-		
-		
+		// Attributes
+		private string retStr = "";
+		private string ATR = "";
+		private string simCommand = "";
+		private string simResponse = "";
+		private string simExpResponse = "";
+		private bool simRespOk = false;
+		private List<string> ADNrecords = new List<string>();
 		
 		
 		
@@ -45,7 +46,9 @@ namespace monosimgtk
 		/// </summary>
 		private void UpdateSelectedReader(string newSelReader)
 		{
-			
+			GlobalObj.CloseConnection();
+			GlobalObj.SelectedReader = newSelReader;
+			StatusBar.Push(1, GlobalObj.LMan.GetString("selreader") + ": " + newSelReader);
 		}
 		
 		
@@ -103,6 +106,8 @@ namespace monosimgtk
 		}
 		
 		
+		
+		
 		private void SaveContactsFile()
 		{
 			
@@ -129,11 +134,135 @@ namespace monosimgtk
 		
 		
 		
-		
+		/// <summary>
+		/// Perform sim card connection and contacts read.
+		/// </summary>
 		private void SimConnect()
 		{
+			MainClass.GtkWait();
+			
+			if (GlobalObj.IsPowered)
+			{
+				// Disconnect card if needed
+				GlobalObj.CloseConnection();
+			}
+			
+			// Connect to smartcard
+			retStr = GlobalObj.AnswerToReset(ref ATR);
+			
+			// check for error
+			if (retStr != "")
+			{
+				// error on answer to reset
+				log.Error("MainWindowClass::SimConnect: " + retStr);
+				MainClass.ShowMessage("ERROR", retStr, MessageType.Error);
+				return;
+			}
+			
+			// read sim contacts and fill list
+			retStr = UpdateSimContactsList();
+			
+			// check for error
+			if (retStr != "")
+			{
+				// error on reading contacts list
+				GlobalObj.CloseConnection();
+				MainClass.ShowMessage("ERROR", retStr, MessageType.Error);
+				return;
+			}
+			
+			
+			// update gui widgets with results
 			
 		}
+		
+		
+		/// <summary>
+		/// Update sim contacts list.
+		/// </summary>
+		private string UpdateSimContactsList()
+		{
+			bool pin1Status = false;
+			string retCmd = GetSimPinStatus(out pin1Status);
+			
+			if (retCmd != "")
+			{
+				// error detected
+				return retCmd;
+			}
+			
+			if (pin1Status)
+			{
+				return GlobalObjUI.LMan.GetString("needpindisable");
+			}
+			
+			
+			return "";
+		}
+		
+		
+		
+		/// <summary>
+		/// Get sim pin1 status (enabled=true or disabled=false)
+		/// </summary>
+		private string GetSimPinStatus(out bool pinStatus)
+		{
+			pinStatus = false;
+			
+			// Select Master File
+			simCommand = "A0A40000023F00";
+			simExpResponse = "9F??";
+			string retCmd = GlobalObjUI.SendReceiveAdv(simCommand, ref simResponse, simExpResponse, ref simRespOk);
+			log.Debug("MainWindowClass::GetSimPinStatus: SELECT MF " + simResponse);
+			
+			if (retCmd != "")
+			{	
+				return retCmd ;
+			}
+			
+			if (!simRespOk)
+			{	
+				return "WRONG RESPONSE [" + simExpResponse + "] " + "[" + simResponse + "]";
+			}
+			
+
+			// Get Response
+			simCommand = "A0C00000" + simResponse.Substring(2,2);
+			simExpResponse = new string('?', (Convert.ToInt32(simResponse.Substring(2,2), 16) * 2)) + "9000";
+			retCmd = GlobalObjUI.SendReceiveAdv(simCommand, ref simResponse, simExpResponse, ref simRespOk);
+			log.Debug("MainWindowClass::GetSimPinStatus: GET RESPONSE " + simResponse);
+			
+			if (retCmd != "")
+			{	
+				return retCmd ;
+			}
+			
+			if (!simRespOk)
+			{	
+				return "WRONG RESPONSE [" + simExpResponse + "] " + "[" + simResponse + "]";
+			}
+			
+			// check for Pin1 status
+			if ( Convert.ToInt32(simResponse.Substring(26, 1), 16) < 8)
+			{
+				// Enabled
+				pinStatus = true;
+			}
+			else
+			{
+				// Disabled
+				pinStatus = false;
+			}
+			
+			return "";
+			
+		}
+		
+		
+		
+		
+		
+		
 		
 		
 		
