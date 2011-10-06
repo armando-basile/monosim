7c491a8cc4b7c8465b04f025cb50bcfa513dbdf5
hmandevteam@gmail.com <hmandevteam@gmail.com@2bb0322c-6031-0410-b042-1f6d2b638ab5>
2011-10-06 21:18:12 +0000

    - added base and resource files

---
+++ trunk/base-components/GlobalConst.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace monosimbase
+{
+	public class GlobalConst
+	{
+		
+		public static readonly string ThanksTo =
+			"\r\n" +
+			"Richard Dale (Kdebindings - Qyoto)\r\n" +
+			"\r\n" +
+			"";
+	}
+}
+
