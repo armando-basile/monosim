4b706e244c6b23651b4cf85b216c11c54f7daceb
hmandevteam <hmandevteam@gmail.com>
2011-10-16 14:43:14 +0000

    - updated link to comex project build section - updated spanish language file by Juan Pablo González Riopedre - updated thanks name list

--- base-components/GlobalConst.cs
+++ base-components/GlobalConst.cs
@@ -8,7 +8,7 @@ namespace monosimbase
 		public static readonly string ThanksTo =
 			"\r\n" +
 			"Richard Dale (richard.j.dale@gmail.com) - Kdebindings, Qyoto\r\n" +
-			"Rigel Va (rigel.va@gmail.com) - spanish translation\r\n" +
+			"Juan Pablo González Riopedre (rigel.va@gmail.com) - spanish translation\r\n" +
 			"Ludovic Rousseau (ludovic.rousseau@gmail.com) - tester\r\n" +
 			"Teddino (teddiman@gmail.com) - tester\r\n" +
 			"Delfino - tester\r\n" +
